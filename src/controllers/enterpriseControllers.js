248a28f9771b1e146088aa770b667cda8898f1c4
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-03-22 20:29:42 -0300

    seguimos en rotas

--- src/controllers/enterpriseControllers.js
+++ src/controllers/enterpriseControllers.js
@@ -1,8 +1,9 @@
 let controller = {
     login: (req, res) => {
         res.render('empresas/login', {title: 'Login EMPRESAS'})
+    },
+    register: (req, res)=>{
+            res.render('empresas/cadastroEmpresa', {title: 'Cadastro EMRESAS'})
     }
-
-
-
+}
 module.exports = controller
