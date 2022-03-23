c8fbd27381481f9539894d586b95a8d4ba22112d
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-03-23 19:21:43 -0300

    committ

--- src/data/listLocales.js
+++ src/data/listLocales.js
@@ -8,15 +8,36 @@ function Locales(localID, comunaID, nameL, addressL, webL, descriptionL, service
         this.servicesL = servicesL,
         this.priceRangeL = priceRangeL,
         this.servicesL = servicesL,
-        this.imgLogo = imgLogo,
         this.img1 = img1,
         this.img2 = img2,
         this.img3 = img3,
         this.img4 = img4,
-
+        this.img5 = img5
 }
 Locales()
 Locales(localID, comunaID, nameL, addressL, webL, descriptionL, servicesL, priceRangeL, servicesL, img1, img2, img3, img4, img5)
 
 local00 = new Locales(localID, comunaID, nameL, addressL, webL, descriptionL, servicesL, priceRangeL, servicesL, img1, img2, img3, img4, img5)
-local01 = newLocales(1, 1, 'Osiris', addressL, webL, descriptionL, servicesL, priceRangeL, servicesL, imgLogo, img1, img2, img3, img4)
+local01 = newLocales(1, 1, 'Osiris', addressL, webL, descriptionL, servicesL, priceRangeL, servicesL, imgLogo, 'https://ibb.co/1RNKwLp', ' https://ibb.co/2Ws9Fzw', 'https://ibb.co/2Ws9Fzw', 'https://ibb.co/0XJKKhV', 'https://ibb.co/4pMnJ3n')
+
+for (comunaID === )
+    let numero = 1;
+
+let numeroTexto = 'Valor desconocido';
+
+switch (comunaID) {
+    case 1:
+        cmn = 'Número uno';
+        break;
+    case 2:
+        lo = 'Número dos';
+        break;
+    case 3:
+        numeroTexto = 'Número tres';
+        break;
+    case 4:
+        numeroTexto = 'Número cuatro';
+        break;
+    default:
+        numeroTexto = 'Caso no encontrado';
+}
