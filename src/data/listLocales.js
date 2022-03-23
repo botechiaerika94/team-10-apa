6e268ecfb7f27842230f87c3d452b967e46fd9fd
BotechiaErika <mendoncabotechiaerikaluisa@gmail.com>
2022-03-23 18:28:22 -0300

    comuna-list-db

---
+++ src/data/listLocales.js
@@ -0,0 +1,22 @@
+function Locales(localID, comunaID, nameL, addressL, webL, descriptionL, servicesL, priceRangeL, servicesL, imgLogo, img1, img2, img3, img4) {
+    this.localID = localID,
+        this.comunaID = comunaID,
+        this.nameL = nameL,
+        this.addressL = addressL,
+        this.webL = webL,
+        this.descriptionL = descriptionL,
+        this.servicesL = servicesL,
+        this.priceRangeL = priceRangeL,
+        this.servicesL = servicesL,
+        this.imgLogo = imgLogo,
+        this.img1 = img1,
+        this.img2 = img2,
+        this.img3 = img3,
+        this.img4 = img4,
+
+}
+Locales()
+Locales(localID, comunaID, nameL, addressL, webL, descriptionL, servicesL, priceRangeL, servicesL, img1, img2, img3, img4, img5)
+
+local00 = new Locales(localID, comunaID, nameL, addressL, webL, descriptionL, servicesL, priceRangeL, servicesL, img1, img2, img3, img4, img5)
+local01 = newLocales(1, 1, 'Osiris', addressL, webL, descriptionL, servicesL, priceRangeL, servicesL, imgLogo, img1, img2, img3, img4)
