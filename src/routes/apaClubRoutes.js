701d980217da25f5a4d30b0a064f73e4ef6ad7f0
Erika Luisa Mendonca Botechia de J. Leite <mendoncabotechiaerikaluisa@gmail.com>
2022-03-25 16:42:16 -0300

    Update apaClubRoutes.js

--- src/routes/apaClubRoutes.js
+++ src/routes/apaClubRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 /* ROUTER PARA COLLECCION MAIN  */
 router.get('/', apaClubController.apaClub)
 router.get('/buscarApa', apaClubController.buscar)
-router.get('/soySocio', apaClubController.miCuenta)
+router.get('/socio', apaClubController.miCuenta)
 
 
 module.exports = router
