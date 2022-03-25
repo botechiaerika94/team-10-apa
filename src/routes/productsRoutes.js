5b9160ba0a32358d6ed622bb3c908bf90b541ce2
Erika Luisa Mendonca Botechia de J. Leite <mendoncabotechiaerikaluisa@gmail.com>
2022-03-25 18:52:23 -0300

    commit

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -2,9 +2,9 @@ const productsController = require('../controllers/productsController.js')
 const express = require('express');
 const router = express.Router();
 
-/* ROUTER PARA COLLECCION MAIN  */
+/* ROUTER PARA COLLECCION PRODUCTS  */
 router.get('/', productsController.landingProducts)
- router.get('/comuna-:comunaID', productsController.comunasP)
- router.get('/comuna-:comunaID/local_id', productsController.detallesL)
+router.get('/comuna-:comunaID', productsController.comunasP)
+router.get('/comuna-:comunaID/:idGID?', productsController.detallesL)
 
-module.exports = router
+module.exports = router;
