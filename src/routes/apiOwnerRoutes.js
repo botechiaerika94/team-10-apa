6eb9bbfb2d34545ffa49677005b63f899b43ba27
Erika Luisa Mendonca Botechia de J. Leite <mendoncabotechiaerikaluisa@gmail.com>
2022-03-28 09:18:37 -0300

    intento

--- src/routes/apiOwnerRoutes.js
+++ src/routes/apiOwnerRoutes.js
@@ -3,5 +3,5 @@ const express = require('express')
 const router = express.Router()
 router.get('/', apiOwnerController.list)
 router.get('/detalles-:register_user', apiOwnerController.read)
-
+    //router.get('/create/:', apiOwnerController.read)
 module.exports = router
