32464c9fa114d00b16399101616e241e7b450dae
Erika Luisa Mendonca Botechia de J. Leite <mendoncabotechiaerikaluisa@gmail.com>
2022-03-28 05:07:35 -0300

    commit Robot

--- src/controllers/apiOwnerControllers.js
+++ src/controllers/apiOwnerControllers.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 const usersFilePath = path.join(__dirname, '../data/usersDataBase.json')
-const usersAll = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+const usersList = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 
 let controller = {
     listUsers: (req, res) => {
         res.render('apiOwner/showUsers', {
             title: 'Lista de Usuarios',
-            users: usersAll
+            usersList: usersList
         });
     }
 }
