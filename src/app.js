fafbbe6b6b07d0a96b959f707888f4c3cd994a3a
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-04-06 19:59:36 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ const usersRouter = require('./routes/usersRoutes.js')
 const productsRouter = require('./routes/productsRoutes.js')
 const enterpriseRouter = require('./routes/enterpriseRoutes.js')
 const apiOwnerRouter = require('./routes/apiOwnerRoutes.js')
+    //const searchRouter = require('./routes/searchRoutes.js')
 
 app.use('/', mainRouters);
 app.use('/apa-club', apaClubRouter);
@@ -30,6 +31,7 @@ app.use('/usuarios', usersRouter);
 app.use('/productos', productsRouter);
 app.use('/empresas', enterpriseRouter);
 app.use('/ApaOwners', apiOwnerRouter);
+//app.use('/buscarApa', searchRouter)
 
 /**************WS LISTEN ON 6669 ***************/
 app.listen(process.env.PORT || 8080, () => {
