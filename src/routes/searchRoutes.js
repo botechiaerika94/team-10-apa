68d124a2cdad5c2387acadce3de4f72591cbb564
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-04-13 19:44:48 -0300

    Update searchRoutes.js

--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -1,7 +1,6 @@
 const searchControllers = require('./../controllers/searchControllers.js')
 const express = require('express')
 const router = express.Router()
-router.get('/categoria?', searchControllers.searchAny)
 router.get('/categoria?', searchControllers.searchZone)
 router.get('/categoria?', searchControllers.searchPrice)
 module.exports = router
