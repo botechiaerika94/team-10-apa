110ad35dfa20114c64041934878eb7c3debfc441
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-04-12 23:34:12 -0300

    ok

--- src/routes/staffRoutes.js
+++
@@ -1,8 +0,0 @@
-const staffController = require('./../controllers/staffControllers.js')
-const express = require('express');
-const router = express.Router();
-
-router.get('/', staffController.index)
-router.get('/registroDiario', staffController.staff01)
-router.get('/dadosStaff/:idStaff', staffController.staff02)
-module.exports = router
