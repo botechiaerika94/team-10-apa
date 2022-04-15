575742e5ef36840774abf517c4dae11f1967762a
botechiaerika <mendoncabotechiaerikaluisa@gmail.com>
2022-04-15 07:24:41 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -39,11 +39,11 @@ app.use('/usuarios', usersRouter);
 app.use('/telos', productsRouter);
 app.use('/empresas', enterpriseRouter);
 app.use('/ApaOwners', apiOwnerRouter);
-app.use('/buscarTelos', searchRouter)
+
 app.use('/ApaGame', gameAIRouter)
 app.use('/staff', staffRouter)
     /***********Server listen 6996****** */
-app.listen(port, () => {
+app.listen(port || 6996, () => {
     console.log('WS LEVANTADO Y CORRIENDO EN 6996')
 })
 module.exports = app()
